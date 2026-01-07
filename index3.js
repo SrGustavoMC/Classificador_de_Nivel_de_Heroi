421e5e0afc3df71af736add538b7d0d3cb9a34f5
SRGustavo <gusmatoscosta@gmail.com>
2026-01-06 21:32:49 -0300

    Alteração no codigo para facilitar a leitura do codigo

--- index3.js
+++ index3.js
@@ -3,23 +3,24 @@ class herois {
         this.nome = nome
         this.idade = idade
         this.tipo = tipo
+    }
 
-    }
     atacar() {
-        while (true) {
+        let ataque
+
         if (this.tipo === 'mago') {
-        return `O ${this.tipo} ${this.nome} de ${this.idade} anos atacou usando magia`
-    } else if (this.tipo === 'guerreiro') {
-        return `O ${this.tipo} ${this.nome} de ${this.idade} anos atacou usando espada`
-    } else if (this.tipo === 'monge') {
-        return `O ${this.tipo} ${this.nome} de ${this.idade} anos atacou usando artes marciais`
-    } else if (this.tipo === 'ninja') {
-        return `O ${this.tipo} ${this.nome} de ${this.idade} anos atacou usando shurikens`
-    } else {
-        return `O ${this.tipo} ${this.nome} de ${this.idade} anos atacou usando shurikens`
+            ataque = 'magia'
+        } else if (this.tipo === 'guerreiro') {
+            ataque = 'espada'
+        } else if (this.tipo === 'monge') {
+            ataque = 'artes marciais'
+        } else if (this.tipo === 'ninja') {
+            ataque = 'shurikens'
+        }
+
+        return `O ${this.tipo} ${this.nome} de ${this.idade} anos atacou usando ${ataque}`
     }
-        }    
-            }
 }
-const heroi1 = new herois('Gandalf',19,'mago')
+
+const heroi1 = new herois('Gandalf', 19, 'mago')
 console.log(heroi1.atacar())
